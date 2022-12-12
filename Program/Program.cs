d95960d27760620fa4a535d86798d6f6ef03bea7
Yana Shushkova <yaya2004@mail.ru>
2022-12-12 21:51:07 +0300

    Program

--- Program/Program.cs
+++ Program/Program.cs
@@ -5,7 +5,7 @@
 При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 */
 
-string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array1 = new string[5] { "123", "23", "hello", "world", "res" };
 string[] array2 = new string[array1.Length];
 
 int len = GetArrayWithIf(array1, array2);
@@ -17,10 +17,10 @@ int GetArrayWithIf(string[] array1, string[] array2)
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
-    if(array1[i].Length <= 3)
+        if (array1[i].Length <= 3)
         {
-        array2[count] = array1[i];
-        count++;
+            array2[count] = array1[i];
+            count++;
         }
     }
     return count;
